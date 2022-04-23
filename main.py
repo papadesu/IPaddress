6d560f69b71c2f40e9b57c442ffedd68e4f644b3
fkzwkzyk <fkzw0815@gmail.com>
2022-04-23 18:05:49 +0900

    subnet error correction

--- main.py
+++ main.py
@@ -7,25 +7,19 @@ streamlit.sidebar.write("""
 ## 数値入力
 """)
 
-first_octet = streamlit.sidebar.number_input('第1オクテット', min_value=0, max_value=255)
-second_octet = streamlit.sidebar.number_input('第2オクテット', min_value=0, max_value=255)
-third_octet = streamlit.sidebar.number_input('第3オクテット', min_value=0, max_value=255)
-fourth_octet = streamlit.sidebar.number_input('第4オクテット', min_value=0, max_value=255)
+first_octet = streamlit.sidebar.number_input('第1オクテット', min_value=0, max_value=255, value=192)
+second_octet = streamlit.sidebar.number_input('第2オクテット', min_value=0, max_value=255, value=168)
+third_octet = streamlit.sidebar.number_input('第3オクテット', min_value=0, max_value=255, value=0)
+fourth_octet = streamlit.sidebar.number_input('第4オクテット', min_value=0, max_value=255, value=0)
 
-subnet = streamlit.sidebar.selectbox(
-    'サブネットマスク',
-    ('/1', '/2', '/3', '/4', '/5', '/6', '/7', '/8'
-    '/9', '/10', '/11', '/12', '/12', '/13', '/14',
-    '/15', '/16', '/17', '/18', '/19', '20', '/21',
-    '/22', '/23', '/24', '/25', '/26', '/27', '/28',
-    '/29', '/30', '/31', '/32'))
+subnet = streamlit.sidebar.number_input('サブネットマスク', min_value=1, max_value=32, value=24)
 
-ip = ip_interface(f'{first_octet}.{second_octet}.{third_octet}.{fourth_octet}{subnet}')
+ip = ip_interface(f'{first_octet}.{second_octet}.{third_octet}.{fourth_octet}/{subnet}')
 network = ip.network.network_address
 
 streamlit.write(f'''
                 ### IPアドレス
-                #### {first_octet}.{second_octet}.{third_octet}.{fourth_octet}{subnet}
+                #### {first_octet}.{second_octet}.{third_octet}.{fourth_octet}/{subnet}
                 ''')
 
 streamlit.write(f'''
